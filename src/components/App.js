11ceeb7e59959aea03528a7bdc48dd61d37712d3
Diego Sarmiento <dlsarmiento@gmail.com>
2022-05-30 22:34:21 -0400

    Added Unit Test

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import CommentBox from './CommentBox';
+import CommentList from './CommentList';
 
-export default() => {
-    return <div>Welcome to my application</div>;
+const App = () => {
+    return <div><CommentBox/><CommentList/></div>;
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import CommentBox from './components/CommentBox';
+import CommentList from './components/CommentList';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App>
+      <CommentBox/>
+      <CommentList/>
+    </App>
   </React.StrictMode>
 );
 
