dfa37a3578b2adad5d7d9be0ebbaebd1fc4b2c7f
Diego Sarmiento <dlsarmiento@gmail.com>
2022-06-01 20:31:05 -0400

    Moved common var to beforeEach

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,20 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from 'components/App';
 
-test('Check Text', () => {
+beforeEach(() => {
   render(<App />);
+})
+
+test('Check Text', () => {
   const linkElement = screen.getByText(/Comment Box/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test('Check Box', () => {
-  render(<App />);
   const commentBox = screen.getByRole('box');
   expect(commentBox).toBeInTheDocument();
 });
 
 test('Check List', () => {
-  render(<App />);
   const commentBox = screen.getByRole('list');
   expect(commentBox).toBeInTheDocument();
 });
